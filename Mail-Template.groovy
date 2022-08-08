130bad013450603d8b0dd779f1ae0d0f9f7f2e8f
Ezgi Nacar <Ezgi.Nacar@tav.aero>
2022-08-08 13:01:14 +0300

    mail template added4

--- Mail-Template.groovy
+++ Mail-Template.groovy
@@ -31,68 +31,40 @@ import groovy.json.JsonSlurper
         </thead>
             <tbody>
 
+                ${workspace}
                 <tr>
-                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build URL : </td>
-        <td style="border: 1px solid #E0DBDB;"><b><a href=${build.url}></a>${build.url}</b></td>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> WORKSPACE : </td>
+        <td style="border: 1px solid #E0DBDB;"><b><a href=${workspace}></a>${workspace}</b></td>
                 </tr>
+
         <tr>
-        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Project URL : </td>
-                    <td style="border: 1px solid #E0DBDB;"><b><a href="${project.url}">${project.url}</a></b></td>
-        </tr>
-                <tr>
-                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build No : </td>
-        <td style="border: 1px solid #E0DBDB;"> ${build.number} </td>
-                </tr>
-        <tr>
-        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Date of build : </td>
-                    <td style="border: 1px solid #E0DBDB;">${it.timestampString}</td>
+        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build URL : </td>
+                    <td style="border: 1px solid #E0DBDB;"><b><a href=${build.url}></a>${build.url}</b></td>
         </tr>
                 <tr>
-                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build duration : </td>
-<td style="border: 1px solid #E0DBDB;">${build.durationString}</td>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Project URL : </td>
+        <td style="border: 1px solid #E0DBDB;"><b><a href="${project.url}">${project.url}</a></b></td>
                 </tr>
         <tr>
-        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Detailed Result : </td>
-                    <td style="border: 1px solid #E0DBDB;"><b><a href="${build.url}"> Click here to see Console Output</a></b></td>
+        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build No : </td>
+                    <td style="border: 1px solid #E0DBDB;"> ${build.number} </td>
         </tr>
-            </tbody>
-</table>
-        <%String jsonString = new File("${build.url}"+"/ws/API/target/surefire-reports/results-json.txt").getText('UTF-8')
-def parser = new JsonSlurper()
-def testsuite = parser.parseText(jsonString);
-def skipped = Integer.valueOf(testsuite.getAt("scenarios")) - (Integer.valueOf(testsuite.getAt("failed")) + Integer.valueOf(testsuite.getAt("passed")))%>
-
-
-        <table style="width:100%; border-collapse: collapse;">
-        <thead style="text-align: center;">
-        <tr style="color:#fff; background-color:#1C4771;">
-        <th style="border: 1px solid #E0DBDB;" colspan="7"><h4><b> Test Summary</b></h4></th>
+                <tr>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Date of build : </td>
+<td style="border: 1px solid #E0DBDB;">${it.timestampString}</td>
                 </tr>
-        <tr style="color:#fff; background-color:#1C4771;">
-        <th style="font-weight:bold; border: 1px solid #E0DBDB;">Test Environment</th>
-
-                    <th style="font-weight:bold; border: 1px solid #E0DBDB;">Total</th>
-        <th style="font-weight:bold; border: 1px solid #E0DDB;">Failed</th>
-                    <th style="font-weight:bold; border: 1px solid #E0DBDB;">Passed</th>
-        <th style="font-weight:bold; border: 1px solid #E0DBDB;">Skipped</th>
-                    <th style="font-weight:bold; border: 1px solid #E0DBDB;">Pass %</th>
-        </tr>
-            </thead>
-        <tbody style="text-align: center;">
-
-
         <tr>
-        <td style="border: 1px solid #E0DBDB;"> <%testsuite.getAt("scenarios")%> </td>
-                    <td style="border: 1px solid #E0DBDB;"> <%testsuite.getAt("scenarios")%> </td>
-        <td style="color:red; border: 1px solid #E0DBDB;"> <%testsuite.getAt("failed")%> </td>
-                    <td style="color:green; border: 1px solid #E0DBDB;"> <%testsuite.getAt("passed")%> </td>
-        <td style="color:#FF7F00; border: 1px solid #E0DBDB;"> <%skipped%> </td>
-                    <td style="border: 1px solid #E0DBDB;"><%(Integer.valueOf(testsuite.getAt("scenarios")) - (Integer.valueOf(testsuite.getAt("failed")) - skipped  * 100)%></td>
-        </tr>
-            </tbody>
+        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build duration : </td>
+                    <td style="border: 1px solid #E0DBDB;">${build.durationString}</td>
+</tr>
+                <tr>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Detailed Result : </td>
+        <td style="border: 1px solid #E0DBDB;"><b><a href="${build.url}"> Click here to see Console Output</a></b></td>
+                </tr>
+</tbody>
         </table>
 
 
-    </div>
-        </body>
+        </div>
+</body>
 </html>
