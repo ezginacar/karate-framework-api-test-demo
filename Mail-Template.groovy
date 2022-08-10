de01910441f488c47342071d82fe1e7aa75243c6
Ezgi Nacar <Ezgi.Nacar@tav.aero>
2022-08-10 12:40:03 +0300

    mail template added6

--- Mail-Template.groovy
+++ Mail-Template.groovy
@@ -14,25 +14,67 @@ import groovy.json.JsonSlurper
 
 <body>
         <div style="width:100%; padding-right:15px; padding-left:15px;margin-right:auto;margin-left:auto; font-family:verdana;">
+        <table style="width:100%; margin-bottom: 1rem; background-color:transparent; border-collapse: collapse;">
+        <thead style="text-align: center; color:#fff; background-color:#1C4771;border-color:#32383e;">
+        <tr>
+        <th style="text-align: center; border-bottom-width: 1px; border: 1px solid #E0DBDB;" colspan="2">
+        <h1>
+        <b>AUTOMATION SUMMARY REPORT</b>
+                        </h1>
+        </th>
+                </tr>
+        <tr>
 
-<%
-def jsonString = new File("${rooturl}${project.url}ws/API/target/surefire-reports/results-json.txt").getText('UTF-8')
-def parser = new JsonSlurper()
-def testsuite = parser.parseText(jsonString)
-def skipped = Integer.valueOf(testsuite.getAt("scenarios")) - (Integer.valueOf(testsuite.getAt("failed")) + Integer.valueOf(testsuite.getAt("passed")))%>
+        <th style="color: <%=build.result == build.result.SUCCESS ? '#008000' : '#FF0000'%>; border-bottom-width: 1px; border: 1px solid #E0DBDB; " colspan="2">
+        <h2><%= build.result == hudson.model.Result.SUCCESS ? "ALL TESTS PASSED" : "SOME TESTS FAILED OR THE JOB ENCOUNTERED A PROBLEM" %></h2>
+                </tr>
+        </thead>
+            <tbody>
 
 
-<table style="width:100%; border-collapse: collapse;">
+                <tr>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Project URL : </td>
+        <td style="border: 1px solid #E0DBDB;"><b><a href="${rooturl}${project.url}">${rooturl}${project.url}</a></b></td>
+                </tr>
+
+        <tr>
+        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build No : </td>
+                    <td style="border: 1px solid #E0DBDB;"> ${build.number} </td>
+        </tr>
+
+                <tr>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build Parameters : </td>
+        <td style="border: 1px solid #E0DBDB;"><b><a href="${rooturl}${build.url}parameters/">See all parameters"</a></b></td>
+        </tr>
+
+                <tr>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Date of build : </td>
+        <td style="border: 1px solid #E0DBDB;">${it.timestampString}</td>
+                </tr>
+        <tr>
+        <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Build duration : </td>
+                    <td style="border: 1px solid #E0DBDB;">${build.durationString}</td>
+        </tr>
+                <tr>
+                    <td style="font-weight:bold; border: 1px solid #E0DBDB;"> Detailed Result : </td>
+<td style="border: 1px solid #E0DBDB;">
+        <b><a href="${rooturl}${build.url}"> Click here to see Console Output</a></b>
+        </td><!--API/target/karate.log-->
+        </tr>
+            </tbody>
+</table>
+
+        <%def xx = "${rooturl}${project.url}"+"/ws/API/target/surefire-reports/results-json.txt"%>
+
+
+        <table style="width:100%; border-collapse: collapse;">
         <thead style="text-align: center;">
         <tr style="color:#fff; background-color:#1C4771;">
         <th style="border: 1px solid #E0DBDB;" colspan="7"><h4><b> Test Summary</b></h4></th>
                 </tr>
         <tr style="color:#fff; background-color:#1C4771;">
         <th style="font-weight:bold; border: 1px solid #E0DBDB;">Total</th>
-                    <th style="font-weight:bold; border: 1px solid #E0DDB;">Failed</th>
-        <th style="font-weight:bold; border: 1px solid #E0DBDB;">Passed</th>
-                    <th style="font-weight:bold; border: 1px solid #E0DBDB;">Skipped</th>
-        <th style="font-weight:bold; border: 1px solid #E0DBDB;">Pass %</th>
+
                 </tr>
         </thead>
             <tbody style="text-align: center;">
@@ -40,13 +82,14 @@ def skipped = Integer.valueOf(testsuite.getAt("scenarios")) - (Integer.valueOf(t
 
                 <tr>
                     <!--Aşağıda Automation_Environment ve Tag_Group değişkenleri build alınmadan önce seçim yaptığım parametre değerlerini temsil ediyor -->
-                    <td style="border: 1px solid #E0DBDB;"> <%testsuite.getAt("scenarios")%> </td>
-        <td style="color:red; border: 1px solid #E0DBDB;"> <%testsuite.getAt("failed")%> </td>
-                    <td style="color:green; border: 1px solid #E0DBDB;"> <%testsuite.getAt("passed")%> </td>
-        <td style="color:#FF7F00; border: 1px solid #E0DBDB;"> <%skipped%> </td>
-                    <td style="border: 1px solid #E0DBDB;"><%(Integer.valueOf(testsuite.getAt("scenarios")) - (Integer.valueOf(testsuite.getAt("failed")) - skipped  * 100)%></td>
+                    <td style="border: 1px solid #E0DBDB;"> <%xx%> </td>
+
         </tr>
             </tbody>
+
+
+
+
 
 
         </div>
